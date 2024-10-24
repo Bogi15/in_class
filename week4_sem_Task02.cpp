3ea9efbf7b946e2a341506d4757c1487cac6044c
Bogomil Pankov <87603630+Bogi15@users.noreply.github.com>
2024-10-24 16:19:23 +0300

    Update week4_sem_Task02.cpp

--- week4_sem_Task02.cpp
+++ week4_sem_Task02.cpp
@@ -1,10 +1,10 @@
 //TASK 2
 #include <iostream>
 int reversingNum(int num){
-    int result=0,copyOfNum=num;
-    while(copyOfNum!=0){
+    int result=0;
+    while(num!=0){
         result = result*10 + copyOfNum%10;
-        copyOfNum /= 10;
+        num /= 10;
     }
     return result;
 }
