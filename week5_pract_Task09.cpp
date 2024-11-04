aa224bcfc9e7b7743fbaf83fda5cb68987fd7f92
Bogomil Pankov <87603630+Bogi15@users.noreply.github.com>
2024-11-04 14:59:27 +0200

    Create week5_pract_Task09.cpp

---
+++ week5_pract_Task09.cpp
@@ -0,0 +1,26 @@
+//ideqta koqto mi kaza momchil da namerq poslednata cifra da q zapazq v previous i da produlja da smqtam ako n > 10 togava namiram poslednata pak i proverqvam dali e ravna na previous
+#include <iostream>
+int kNumbersLength(int k){
+    int length=0;
+    while(k!=0){
+        length++;
+        k /= 10;
+    }
+    return length;
+}
+int sameNeigbouringDigits(int k){
+    while(length-1!==0){
+        
+    }
+}
+void run(){
+  int k,l;
+  std::cin>>k>>l; 
+  for(int i=k;k<=l;i++){
+      
+  }
+}
+int main() {
+ 
+  
+}
